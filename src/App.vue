7b8314b60bdcc09ede24a2d685d3d2f9649acf75
osocaramelosofer <osocaramelosofer@hotmail.com>
2023-03-15 10:03:02 -0600

    122.- Navegar entre paginas - router link

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
+  <Navbar />
   <img alt="Vue logo" src="./assets/logo.png">
   <router-view />
 </template>
 
 <script>
 
+import { defineAsyncComponent } from 'vue'
+
 export default {
   name: 'App',
-  components: {  }
+  components: { 
+    Navbar: defineAsyncComponent( () => import(/* webpackChunkName: "Navbar"*/ "@/modules/shared/components/Navbar"))
+  }
 }
 </script>
 
---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <router-link to="/">Pokemon List</router-link>
+    <router-link to="/id">Pokemon id</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+div{
+padding: 0 30px;    
+}
+div a {
+    font-weight: bold;
+    color: #2c3e50;
+    margin: 0 10px;
+}
+</style>

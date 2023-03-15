3b95639edc20b4c2ff2b5632a6fde2a4c1beb650
osocaramelosofer <osocaramelosofer@hotmail.com>
2023-03-15 09:40:53 -0600

    121.-LazyLoad de paginas vue router

--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,22 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import AboutPage from "@/modules/pokemon/pages/AboutPage"
-import ListPage from "@/modules/pokemon/pages/ListPage"
-import PokemonPage from "@/modules/pokemon/pages/PokemonPage"
-
 const routes = [
-    { path: '/', component: ListPage },
-    { path: '/about', component: AboutPage},
-    { path: '/id', component: PokemonPage}
+    {
+        path: '/',
+        component: () => import(/* webpackChunckName: "ListPage"*/ "@/modules/pokemon/pages/ListPage")
+    },
+    { 
+        path: '/about',
+        component: () => import(/* webpackChunckName: "AboutPage"*/ "@/modules/pokemon/pages/AboutPage") 
+    },
+    { 
+        path: '/id',
+        component: () => import(/* webpackChunckName: "PokemonPage"*/ "@/modules/pokemon/pages/PokemonPage") 
+    },
+    { 
+        path:'/:pathMatch(.*)*',
+        component: () => import(/* webpackChunckName: "NotFoundPage"*/ "@/modules/shared/pages/NoPageFound")
+    },
 ]
 
 const router = createRouter({

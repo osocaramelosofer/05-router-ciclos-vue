8ec9e1a0ffff59d264195eb3d07d8167c2c891dd
osocaramelosofer <osocaramelosofer@hotmail.com>
2023-03-15 09:42:05 -0600

    121.-LazyLoad de paginas vue router

--- src/router/router.js
+++ src/router/router.js
@@ -3,19 +3,19 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
     {
         path: '/',
-        component: () => import(/* webpackChunckName: "ListPage"*/ "@/modules/pokemon/pages/ListPage")
+        component: () => import(/* webpackChunkName: "ListPage"*/ "@/modules/pokemon/pages/ListPage")
     },
     { 
         path: '/about',
-        component: () => import(/* webpackChunckName: "AboutPage"*/ "@/modules/pokemon/pages/AboutPage") 
+        component: () => import(/* webpackChunkName: "AboutPage"*/ "@/modules/pokemon/pages/AboutPage") 
     },
     { 
         path: '/id',
-        component: () => import(/* webpackChunckName: "PokemonPage"*/ "@/modules/pokemon/pages/PokemonPage") 
+        component: () => import(/* webpackChunkName: "PokemonPage"*/ "@/modules/pokemon/pages/PokemonPage") 
     },
     { 
         path:'/:pathMatch(.*)*',
-        component: () => import(/* webpackChunckName: "NotFoundPage"*/ "@/modules/shared/pages/NoPageFound")
+        component: () => import(/* webpackChunkName: "NotFoundPage"*/ "@/modules/shared/pages/NoPageFound")
     },
 ]
 

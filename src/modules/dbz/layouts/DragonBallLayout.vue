ae1aa4bcd77e4f0e5701ceac4713a32102dd613a
osocaramelosofer <osocaramelosofer@hotmail.com>
2023-03-16 10:24:29 -0600

    132.- Segundo layout

---
+++ src/modules/dbz/layouts/DragonBallLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="dbz-layout">
+    <h1>Dragon ball Layout</h1>
+    <router-view />
+  </div>
+</template>
+<style scoped>
+.dbz-layout{
+  background-color: cornflowerblue;
+}
+</style>
---
+++ src/modules/dbz/pages/Characters.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Characters</h1>
+  </div>
+</template>
